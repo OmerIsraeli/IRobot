adf9541b575b6a68de50d6aeee96275d274e0a1c
noamscohen <noamshaked.cohen.41@gmail.com>
2021-05-13 00:37:18 +0300

    working client

--- wifi_client1.1.py
+++ wifi_client1.1.py
@@ -1,9 +1,11 @@
+
+
 import socket
 import time
 import pickle
 import numpy as np
 
-address = "132.64.143.30"
+address = "132.64.143.249"
 msgFromClient = "Hello UDP Server"
 bytesToSend = str.encode(msgFromClient)
 serverAddressPort = (address, 20001)
@@ -21,18 +23,18 @@ import time
 
 # Set up pygame and the display
 os.putenv('SDL_FBDEV', '/dev/fb1')
-# pygame.init()
-# lcd = pygame.display.set_mode((320, 240))
-# pygame.mouse.set_visible(False)
-# lcd.fill((0, 0, 0))
-# pygame.display.update()
+pygame.init()
+lcd = pygame.display.set_mode((320, 240))
+pygame.mouse.set_visible(False)
+lcd.fill((0, 0, 0))
+pygame.display.update()
 
 # Setup the RPLidar
 PORT_NAME = '/dev/ttyUSB0'
 lidar = Lidar(None, PORT_NAME)
 # used to scale data to fit on the screen
 max_distance = 0
-SIZE_POCKET = 200
+SIZE_POCKET=200
 scan_data = [0] * 360
 
 
@@ -41,10 +43,8 @@ def process_data(data):
     pass
 
 
-def sent_data(data):
-    #     data=concat_dist_angles(distances, angles)
-
-    #     UDPClientSocket.sendall(('%6d'.encode() % len(data)), serverAddressPort )
+def sent_data(distances, angles):
+    data=concat_dist_angles(distances, angles)
     UDPClientSocket.sendto(data, serverAddressPort)
     # print(i)
     # i += 1
@@ -52,22 +52,19 @@ def sent_data(data):
 
 def main():
     try:
-        # print(lidar.get_info())
+        #print(lidar.get_info())
         for scan in lidar.iter_scans():
             #     for (_, angle, distance) in scan:
             #         scan_data[min([359, floor(angle)])] = distance
             #     process_data(scan_data)
-            #             NOR=np.random.choice(len(scan),min(SIZE_POCKET,len(scan)),replace=False)
+            NOR=np.random.choice(len(scan),min(SIZE_POCKET,len(scan)),replace=False)
 
-            #             new_scan = np.array(scan)[NOR,:]
+            new_scan = np.array(scan)[NOR,:]
 
-            #             distances = [item[2] for item in scan]
-            #             angles = [item[1] for item in scan]
-            new_scan = np.array([(item[1], item[2]) for item in scan])
-            #             print(len(distances))
-            print(new_scan)
-            #             data = np.array((distances,angles))
-            sent_data(new_scan.tobytes())
+            distances = [item[2] for item in new_scan]
+            angles = [item[1] for item in new_scan]
+            print(len(distances))
+            sent_data(distances, angles)
 
     except KeyboardInterrupt:
         print('Stopping.')
@@ -76,7 +73,7 @@ def main():
 
 
 def concat_dist_angles(distances, angles):
-    arr = (distances, angles)
+    arr = (distances,angles)
     data_string = pickle.dumps(arr)
     return data_string
 
